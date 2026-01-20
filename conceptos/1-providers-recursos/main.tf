86e5ce7803e5c1bb5f48bf956270c51b08da5426
Iván Osuna Ayuste <ivan@IMACPRO-D6BDA9.station>
2026-01-20 10:13:44 +0100

    Dia 2

--- conceptos/1-providers-recursos/main.tf
+++ conceptos/1-providers-recursos/main.tf
@@ -6,11 +6,42 @@
 #   el proveedor.
 # }
 
+# El NOMBRE_DEL_RECURSO es un identificador que nosotros ponemos 
+# para poder referenciar este recurso dentro de nuestro código.
+# A la hora de referenciarlo, usamos: TIPO_DE_RECURSO.NOMBRE_DEL_RECURSO
+  
 # Por ejemplo, si quisiéramos crear un contenedor Docker, con nginx
 
 resource "docker_container" "mi_contenedor" {
-    name  = "mi-nginx"
-    image = docker_image.mi_imagen.image_id # De ahí estoy sacando el ID de la imagen
+    # Las propiedades que aquí vamos definiciendo vienen determinadas por el provider (docker en este caso)
+    # En la documentación del provider me informan de todas las propiedades que puedo usar y de sus tipos 
+    # de datos.
+    # La sintaxis que usaremos a la hora de definir una propiedad depende del tipo de dato de esa propiedad.
+                 # Los textos (String) van entre comillas
+    name       = "mi-nginx"
+    image      = docker_image.mi_imagen.image_id # De ahí estoy sacando el ID de la imagen
+                 # Esto también es un texto que estamos rellenando desde un dato de otro recurso
+                 # En la referencia no usmos comilla. Si le pongo comillas se entendería que es un texto literal.
+    cpu_shares = 512          # Un número (NUMBER). En este caso, medio core. 
+                              # Los números sin comillas
+    start      = true         # Un booleano (BOOLEAN). true o false, sin comillas
+    env        = ["VAR1=valor1" , "VAR2=valor2"] # Esto serían los Set y los List
+
+    # Cuando encuentro un Block List o un Block Set la sintaxis cambia
+    # Detras del nombre de la propiedad ponemos llaves
+    # Dentro de las llaves las propiedades que me defina la documentación
+    # Si quiero definir varios bloques, los ponemos uno detrás de otro, con la misma sintaxis
+    ports {
+        internal = 80
+        external = 8080
+        ip       = "0.0.0.0"
+    }
+
+    ports {
+        internal = 443
+        external = 8443
+    }
+
 }
 
 
--- conceptos/1-providers-recursos/versions.tf
+++ conceptos/1-providers-recursos/versions.tf
@@ -18,6 +18,24 @@ terraform {
     docker = {
       source = "kreuzwerker/docker"
       version = "3.6.2"
+        # De qué depende la versión que quiero usar?
+        # - Compatibilidad con mi versión de Terraform
+        # - Las funcionalidades que necesito para mi script.
+        #   Cada versión de un provider aporta una serie de funcionalidades.
+        #   Si una versión ya trae las funcionalidades que necesito. ME VALE !! No la toques. 
+        #   No necesito una versión superior
+        # - Subir de versión implica SERIOS RIESGOS! Pueden venir nuevos BUGS
+        #   Tengo además que prestar especial atención a no subir de major a lo loco (sin pruebas muy extensivas)
+        #   El Major se incrementa cuando hay cambios incompatibles con versiones anteriores = BREAKING CHANGES
+        #   Al subir un major, se me puede caer el chiringuito! Me puede dejar de funcionar lo que tenemos hecho.
+        # Una opción muy conservadora es poner una versión fija, como tenemos aquí: 3.6.2
+        # Una opción muy aceptable es poner un rango con el operador ~> , que nos permite actualizaciones de parches y menores
+        #      version = "~> 3.6.2" Esto nos instalaría el último patch de la versión 3.6.x. Si sale una 3.7 esa no se instalaría.
+        # Otras opciones que podemos usar son >, <, >=, <=. Incluso concatenarlas:
+        #      version = ">= 3.6.2, < 4.0.0" Esto nos permitiría actualizaciones de parches y menores, pero no de major.
+        #       En este caso, estamos fijando el major a la versión 3.
+        # Podría ser aceptable en alguos casos (es menos conservador) que fijar solamente el patch.
+        # Ni de coña deberíamos usar operadores como > o >= sin más restricciones, ya que nos exponemos a actualizaciones de major
     }
   }
 }
